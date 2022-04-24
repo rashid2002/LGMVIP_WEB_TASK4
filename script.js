de0c751073d075cdf70a81e29589909a493bcafc
Rashid <rashid99ansariy2j@gmail.com>
2022-04-24 17:42:45 +0530

    lgm

---
+++ script.js
@@ -0,0 +1,26 @@
+
+let outputScreen = document.getElementById("output_screen");
+function display(num) {
+  outputScreen.value += num;
+}
+function calculate() {
+  try {
+    outputScreen.value = eval(outputScreen.value);
+  } catch (err) {
+    outputScreen.value = "Error";
+  }
+}
+function Clear() {
+  outputScreen.value = "";
+}
+function del() {
+  outputScreen.value = outputScreen.value.slice(0, -1);
+}
+
+// function appendToOutputScreen()
+// {
+//     console.log("1")
+//     let buttonText =document.querySelector('.btn').innerText;
+//     outputScreen.value += buttonText;
+//     console.log(buttonText);
+// }
